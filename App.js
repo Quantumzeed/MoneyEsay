dde3ebc6c479d6d756d5275d3595b8342ca24031
Quantumzeed <87402893+Quantumzeed@users.noreply.github.com>
2022-10-11 11:01:07 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './src/screens/Home'
+import Navigation from './src/navigations/Navigation.JS'
+
+const App = () => {
+  return (
+    <Navigation />
+  )
+}
+
+export default App
